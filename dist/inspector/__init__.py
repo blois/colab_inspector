9d5b820569b7341e90cd2f02b4c0b80b4203b3cf
Pete Blois <pete@blois.us>
2017-12-12 22:15:10 -0600

    Tue Dec 12 22:15:09 CST 2017

--- dist/inspector/__init__.py
+++ dist/inspector/__init__.py
@@ -1,2 +1,11 @@
 def inspect():
   return 'inspected'
+
+def _jupyter_nbextension_paths():
+  # See:
+  # http://testnb.readthedocs.io/en/latest/examples/Notebook/Distributing%20Jupyter%20Extensions%20as%20Python%20Packages.html#Defining-the-server-extension-and-nbextension
+  return [{
+      'dest': 'google.colab.inspector',
+      'section': 'notebook',
+      'src': 'resources',
+  }]
